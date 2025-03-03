5228092562a26f5e43a05838c937da52432d7ec2
Paghdar Param <124564382+parax22@users.noreply.github.com>
2025-03-03 17:41:04 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,7 @@
+<hr class="footerline"><!--css modified horizontal line-->
+<footer>
+    <div class="container">
+        <h5 style="text-align: center;">&copy; 2025 BY: Event Menagment</h5>
+        <br>
+    </div>
+</footer>
---
+++ utils/header3.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Preranothsava</title>
+    <style>
+        .bgImage {
+            background-color: blueviolet;
+            background-size: cover;
+            background-position: center center;
+            height: 50vh;
+            margin-bottom: 25px;
+        }
+        h1
+        {
+            color: white;
+            font-size: 80px;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            margin-top: 200px;
+            margin-left: 180px;
+        }
+    </style>
+</head>
+<body>
+    <header class="bgImage">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-header"><!-- Website name/title can be added here if needed -->
+                </div>
+                <ul class="nav navbar-nav navbar-right"><!-- Navigation links -->
+                    <li><a href="./index.php"><strong>Home</strong></a></li>
+                  <!--  <li><a href="./usn.php"><strong>Already registered</strong></a></li>-->
+                    <li><a href="./contact.php"><strong>Contact Us</strong></a></li>
+                    <li><a href="./aboutus.php"><strong>About Us</strong></a></li>
+                    <li class="btnlogout"><a class="btn btn-default navbar-btn" href="./login_form.php">Login <span class="glyphicon glyphicon-log-in"></span></a></li>
+                </ul>
+            </div><!-- /.container -->
+        </nav>
+        <div class="slogn">
+            <h1>Enjoy & Fun Here</h1>
+        </div>
+        <!-- Optional: Jumbotron and browse section can be added here if needed -->
+    </header>
+</body>
+</html>
